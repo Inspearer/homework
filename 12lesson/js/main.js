261b889ad02767eedf8adc10f414813f5a38ad07
Inspearer <rusteha05@mail.ru>
2025-08-17 19:09:16 +0500

    Add files via upload

--- 12lesson/js/main.js
+++ 12lesson/js/main.js
@@ -16,3 +16,19 @@
       document.body.classList.remove('body--opened-menu')
   }
 })()
+
+const openModal = document.querySelector('.gift--link')
+const body = document.body
+openModal.addEventListener('click', event => {
+  event.preventDefault();
+  body.classList.add('body--opened-modal')
+})
+
+const modalWindow = document.querySelector('.modal')
+modalWindow.addEventListener('click', event => {
+  const target = event.target
+  event.preventDefault();
+  if (target && target.classList.contains('modal')||target.closest('.modal__cancel')) {
+    body.classList.remove('body--opened-modal')
+  }
+})
